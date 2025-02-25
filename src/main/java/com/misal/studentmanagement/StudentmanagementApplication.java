e4f1cffede3ba8953775cb3b86d0f4b3ff45dafc
Mikail Huseynov <60282641+mikailhs@users.noreply.github.com>
2025-02-25 17:51:26 +0400

    add olundu

--- src/main/java/com/misal/studentmanagement/StudentmanagementApplication.java
+++ src/main/java/com/misal/studentmanagement/StudentmanagementApplication.java
@@ -8,6 +8,7 @@ public class StudentmanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentmanagementApplication.class, args);
+		System.out.println("Salaaaam");
 	}
 
 }
